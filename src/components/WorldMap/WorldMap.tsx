1a88ae4816f4372a2ca86856c0380cbad093e4c6
AxelMBC <barrazaaxel2000@gmail.com>
2025-06-01 22:31:46 -0600

    funcion onClick en el mapa funciona correctamente

--- src/components/WorldMap/WorldMap.tsx
+++ src/components/WorldMap/WorldMap.tsx
@@ -1,22 +1,32 @@
-// Using Maplibre
-import Map from 'react-map-gl/maplibre';
-import 'maplibre-gl/dist/maplibre-gl.css';
+import  { useRef } from "react";
+import Map from "react-map-gl/maplibre";
+import type { MapRef } from "@vis.gl/react-maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
 
-const  WorldMap = () => {
+const WorldMap = () => {
+  const mapRef = useRef<MapRef | null>(null);
+
   return (
     <Map
+      ref={mapRef}
       initialViewState={{
         longitude: 0,
         latitude: 21.8,
         zoom: 1,
-        
       }}
       minZoom={1}
-      style={{width: 1000, height: 667}}
+      style={{ width: 1000, height: 667 }}
       mapStyle="https://api.maptiler.com/maps/0197251e-f92a-7cb9-98e8-774bde6e5d8e/style.json?key=FcmP3QUhWP2LWWud2CSk"
       renderWorldCopies={false}
+      onDblClick={(event) => {
+        if (mapRef.current) {
+          const map = mapRef.current.getMap();
+          const features = map.queryRenderedFeatures(event.point);
+          console.log("Double-clicked Country:", features[0].layer.id);
+        }
+      }}
     />
   );
-}
+};
 
 export default WorldMap;
