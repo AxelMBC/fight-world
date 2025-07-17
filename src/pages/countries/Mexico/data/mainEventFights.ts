1b72cccfeea5cf6c6444813d7634713942b9599d
El Coflas <barrazaaxel2000@gmail.com>
2025-07-16 21:48:03 -0600

    feat: :seedling: Se agregaron peleas main event a Mexico
    8 peleas totales

--- src/pages/countries/Mexico/data/mainEventFights.ts
+++ src/pages/countries/Mexico/data/mainEventFights.ts
@@ -34,4 +34,58 @@ export const mainEventFights: mainEventType[] = [
     fighterBlue: "Ricardo Lopez",
     fighterRed: "Hideyuki Ohashi",
   },
+  {
+    id: 3,
+    title: "Saul Canelo Alvarez; Sparring - Complete & Unedited",
+    description:
+      "Sesión de sparring de Canelo preparándose para Josesito Lopez, mostrando su defensa, relajación y precisión en sus golpes.",
+    idYt: "lTCBaL2ccPI",
+    startTime: "0",
+    tags: ["Sparring", "Contraataque", "Preparación de Pelea"],
+    fighterBlue: "Saúl 'Canelo' Álvarez",
+  },
+  {
+    id: 4,
+    title: "Will Grigsby vs Ricardo Lopez [02-10-1999] [Vía Digital]",
+    description:
+      "Pelea por el Título Mundial IBF Peso Mini Mosca entre Ricardo 'Finito' López y Will Grigsby desde Las Vegas, Nevada.",
+    idYt: "x77u20vxhmU",
+    startTime: "77",
+    tags: ["Pelea de Campeonato", "Boxeo Clásico", "Leyenda Mexicana"],
+    fighterBlue: "Ricardo 'Finito' López",
+    fighterRed: "Will Grigsby",
+  },
+  {
+    id: 5,
+    title: 'Ricardo "Finito" Lopez vs Zolani Petelo',
+    description:
+      "La pelea de retiro de Ricardo López, donde demuestra por qué es una de las máximas joyas del boxeo mexicano.",
+    idYt: "NA65p34TdXU",
+    startTime: "1",
+    tags: ["Pelea de Retiro", "Maestro Técnico", "Leyenda del Boxeo"],
+    fighterBlue: "Ricardo 'Finito' López",
+    fighterRed: "Zolani Petelo",
+  },
+  {
+    id: 6,
+    title: "Ricardo Lopez vs Will Grigsby - Full Fight",
+    description:
+      "Pelea por el título mosca junior de la IBF. Will Grigsby presenta una sólida y competitiva batalla contra la leyenda Ricardo 'Finito' López.",
+    idYt: "7jASSHmHUZw",
+    startTime: "150",
+    tags: ["Pelea Competitiva", "Título Mundial", "Boxeo Clásico"],
+    fighterBlue: "Ricardo 'Finito' López",
+    fighterRed: "Will Grigsby",
+  },
+  {
+    id: 7,
+    title: "Tim Bradley Jr. vs. Juan Marquez | Full Fight",
+    description:
+      "Épica pelea de 2013 entre dos grandes guerreros, Timothy Bradley y la leyenda mexicana Juan Manuel Márquez en un combate reñido.",
+    idYt: "VO13QnNCrrs",
+    startTime: "26",
+    tags: ["Duelo de Leyendas", "Guerra en el Ring", "Orgullo Mexicano"],
+    fighterBlue: "Timothy 'Desert Storm' Bradley Jr.",
+    fighterRed: "Juan Manuel 'Dinamita' Márquez",
+  },
 ];
