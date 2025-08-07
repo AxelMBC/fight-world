1e4399e4646d0b22cb32174cd41a54d84a3fe3a2
El Coflas <barrazaaxel2000@gmail.com>
2025-08-06 22:23:00 -0600

    feat: :sparkles: Bandera Mexicana actualizada para mostrar simbolo

--- src/pages/countries/Mexico/resources/Flag.tsx
+++ src/pages/countries/Mexico/resources/Flag.tsx
@@ -1,7 +1,13 @@
+import SimboloBandera from "./Simbolo_Bandera.svg";
+
 const Flag = (
   <>
     <span className="w-16 h-8 bg-[#006847]"></span>
-    <span className="w-16 h-8 bg-white border-2 border-black"></span>
+
+    <span className="w-16 h-8 bg-white border-2 border-black flex items-center justify-center relative overflow-hidden">
+      <img src={SimboloBandera} alt="Bandera de México" className="w-10 h-auto" />
+    </span>
+
     <span className="w-16 h-8 bg-[#CE1126]"></span>
   </>
 );
