05f421ab087ce4e8ae529a9a08f26464d18bea9f
El Coflas <barrazaaxel2000@gmail.com>
2025-09-16 22:34:36 -0600

    cambio menor, se quito bandera

--- src/components/HeaderTitle/HeaderTitle.tsx
+++ src/components/HeaderTitle/HeaderTitle.tsx
@@ -7,9 +7,9 @@ const HeaderTitle = ({ flag, title }: HeaderTitleProps) => {
   return (
     <>
       <header className="text-center header-title ">
-        <div className="flex justify-center items-center space-x-4 mt-1 mb-4">
+        {/* <div className="flex justify-center items-center space-x-4 mt-1 mb-4">
           {flag}
-        </div>
+        </div> */}
         <h1 className="title-main fc-primary-dark pt-2 uppercase">
           {title}
         </h1>
